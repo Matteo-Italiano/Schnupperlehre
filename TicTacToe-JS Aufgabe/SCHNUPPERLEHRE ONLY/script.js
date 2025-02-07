9956406bddacf37d132219733a964efb398bdd14
Matteo Italiano <matteo.italiano@3ap.ch>
2025-02-07 11:07:15 +0100

    Final Version ig

--- TicTacToe-JS Aufgabe/SCHNUPPERLEHRE ONLY/script.js
+++ TicTacToe-JS Aufgabe/SCHNUPPERLEHRE ONLY/script.js
@@ -1,64 +1,33 @@
-let currentplayer = "X";
-let playerlabel = document.getElementById("playerlabel")
+let playerlabel = document.getElementById("playerlabel");
 
-function setplay(index) {
-  let indexplayed = document.getElementById(index);
-
-  if (indexplayed.innerText == "") {
-    indexplayed.innerText = currentplayer;
-    if (checkwinner()){
-      changeturn();
-      playerlabel.textContent = `Spieler ${currentplayer} ist dran`
-    }
-    
-  } else {
-    // Feld Schon besetzt
-  }
-}
-
+// Funktion erstellen welche den Spielzug aufnimmt
 
 // Funktion Erstellen Welche den Jetztigen Spieler wechselt
 
-
-
-
-
-
-
-
 // Funktion erstellen welche das Spiel Zurücksetzt
 
+// Funktion erstellen, welche den
 
-
-
-
-
-
-
-// Funktion erstellen, welche den 
-
-function checkwinner(){ 
+function checkwinner() {
   let winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontal
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Vertical
-    [0, 4, 8], [2, 4, 6] // Diagonal
-];
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Horizontal
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Vertical
+    [0, 4, 8],
+    [2, 4, 6], // Diagonal
+  ];
 
-for (let combination of winningCombinations){
-  let [a, b, c,] = combination
+  for (let combination of winningCombinations) {
+    let [a, b, c] = combination;
 
-  let cellA = document.getElementById(a.toString()).innerText;
-  let cellB = document.getElementById(b.toString()).innerText;
-  let cellC = document.getElementById(c.toString()).innerText;
+    let cellA = document.getElementById(a.toString()).innerText;
+    let cellB = document.getElementById(b.toString()).innerText;
+    let cellC = document.getElementById(c.toString()).innerText;
 
-  if (cellA !== "" && cellA === cellB && cellA === cellC){
-    playerlabel.textContent = `Spieler ${currentplayer} gewinnt`
-    currentplayer = ""
-    return false
+    if (cellA !== "" && cellA === cellB && cellA === cellC) {
+    }
   }
 }
-
-return true
-}
-
-
